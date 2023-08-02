0b2808d64fb57adbfd34916cce509d694844cb27
Nicolas Hrubec <nico.hrubec@mongodb.com>
2023-08-02 16:12:25 +0100

    Add third parameter to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $1 == "projects" ]]; then
-  echo "Project create"
+if [[ $1 == "projects" && $2 == "create" ]]; then
+  atlas projects create "$3"
+else
+  atlas "$1" "$2" "$3"
 fi
-
-atlas "$1" "$2"
