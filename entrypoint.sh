965d3adbb82fe68ee984ecf2f1e55b2673616d00
Nicolas Hrubec <nico.hrubec@mongodb.com>
2023-08-02 17:00:16 +0100

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "projects" && $2 == "create" ]]; then
-  projectId=(atlas projects create "$3" -o json-path="$.id")
+  projectId=$(atlas projects create "$3" -o json-path="$.id")
   echo "projectId=$projectId" >> $GITHUB_OUTPUT
 else
   atlas "$1" "$2" "$3"
